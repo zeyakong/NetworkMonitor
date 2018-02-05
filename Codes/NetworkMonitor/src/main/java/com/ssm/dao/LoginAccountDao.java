7cbecbf96a36b25402f5471a26efba4d28909e5c
kong.zeya <kong.zeya@uwlax.edu>
2018-02-04 23:29:52 -0600

    add test

--- Codes/NetworkMonitor/src/main/java/com/ssm/dao/LoginAccountDao.java
+++ Codes/NetworkMonitor/src/main/java/com/ssm/dao/LoginAccountDao.java
@@ -1,8 +1,12 @@
 package com.ssm.dao;
 
+import com.ssm.entity.LoginAccount;
+import org.apache.ibatis.annotations.Param;
+
 /**
  * Created by Zeya Kong
  * On 2018/1/31 15:50.
  */
 public interface LoginAccountDao {
+    LoginAccount findAdminByIdAndPassword(@Param("username")String username, @Param("password")String password);
 }
