197bab46df11e30422a832e2856bfb5bd744aa5d
kong.zeya <kong.zeya@uwlax.edu>
2018-02-04 22:40:47 -0600

    add test

--- Codes/NetworkMonitor/src/main/java/com/ssm/dao/AdminDao.java
+++ Codes/NetworkMonitor/src/main/java/com/ssm/dao/AdminDao.java
@@ -5,5 +5,4 @@ package com.ssm.dao;
  * On 2018/1/31 15:50.
  */
 public interface AdminDao {
-      Admin findAdminByUsernameAndPassword(@Param("username")String username, @Param("password")String password);
 }
--- Codes/NetworkMonitor/src/main/java/com/ssm/entity/LoginAccount.java
+++ Codes/NetworkMonitor/src/main/java/com/ssm/entity/LoginAccount.java
@@ -3,6 +3,6 @@ package com.ssm.entity;
 /**
  * Created by Zeya Kong
  * On 2018/1/31 15:49.
-        */
+ */
 public class LoginAccount {
 }
