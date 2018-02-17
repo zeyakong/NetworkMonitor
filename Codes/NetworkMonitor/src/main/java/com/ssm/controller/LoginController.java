15ce40932da38bfa064a0add606964d5716a0a57
kong.zeya <kong.zeya@uwlax.edu>
2018-02-17 12:17:20 -0600

    Plz do not add the prefix in the xml file.

--- Codes/NetworkMonitor/src/main/java/com/ssm/controller/LoginController.java
+++ Codes/NetworkMonitor/src/main/java/com/ssm/controller/LoginController.java
@@ -29,7 +29,7 @@ public class LoginController {
 
     @RequestMapping("/login")
     public String goLoginPage() {
-        return "login.jsp";
+        return "login";
     }
 
     @RequestMapping("/doLogin")
