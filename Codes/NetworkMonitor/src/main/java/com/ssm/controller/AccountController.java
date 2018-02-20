b9b1bd90ada0ad644e02cacb1162d060de47248c
kong.zeya <kong.zeya@uwlax.edu>
2018-02-20 13:57:22 -0600

    demo2 integration

--- Codes/NetworkMonitor/src/main/java/com/ssm/controller/AccountController.java
+++ Codes/NetworkMonitor/src/main/java/com/ssm/controller/AccountController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import java.util.List;
 
 /**
@@ -25,12 +26,12 @@ public class AccountController {
     private CreditCardServices creditCardServices;
 
     @RequestMapping("/accountCards")
-    public String accountCards(int id,Model model){
+    public String accountCards(int id, Model model) {
         List<CreditCard> list;
         list = creditCardServices.findCreditCardsByAccountId(id);
-        model.addAttribute("list",list);
+        model.addAttribute("list", list);
         CardAccount cardAccount = cardAccountServices.findCardAccountById(id);
-        model.addAttribute("cardAccount",cardAccount);
+        model.addAttribute("cardAccount", cardAccount);
         return "accountDetail";
     }
 }
