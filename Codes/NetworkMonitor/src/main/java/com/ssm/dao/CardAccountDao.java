41714ecb2a0c780b1f066a26cf9fc5a6a8244491
SherryKitsch <35751098+SherryKitsch@users.noreply.github.com>
2018-03-28 01:13:56 +0800

    Add files via upload

--- Codes/NetworkMonitor/src/main/java/com/ssm/dao/CardAccountDao.java
+++ Codes/NetworkMonitor/src/main/java/com/ssm/dao/CardAccountDao.java
@@ -2,7 +2,6 @@ package com.ssm.dao;
 
 import com.ssm.entity.CardAccount;
 import org.apache.ibatis.annotations.Param;
-
 import java.util.List;
 
 /**
@@ -15,11 +14,13 @@ public interface CardAccountDao {
 
     List<CardAccount> findAllCardAccounts();
     CardAccount findCardAccountById(int id);
-    void createCardAccount(@Param("name")String name,
-                           @Param("address")String address, @Param("limit")double limit,
-                           @Param("phone")String phone, @Param("balance")double balance);
-    void updateCardAccount(@Param("id")int id,@Param("name")String name,
-                           @Param("address")String address, @Param("limit")double limit,
-                           @Param("phone")String phone, @Param("balance")double balance);
-    void deleteCardAccount(int id);
+    void xxx();
+    void zzz();
+ /*   void createaccount(String name,String address,int phone,double limit,double balance);*/
+  void createaccount(@Param("name")String name,
+                           @Param("address")String address, @Param("limit")String limit,
+                           @Param("phone")String phone, @Param("balance")String balance);
+    void deleteCardAccountById(int id);
+    int getmax();
 }
+
--- Codes/NetworkMonitor/src/main/java/com/ssm/dao/CreditCardDao.java
+++ Codes/NetworkMonitor/src/main/java/com/ssm/dao/CreditCardDao.java
@@ -4,7 +4,6 @@ import com.ssm.entity.CardAccount;
 import com.ssm.entity.CreditCard;
 import org.apache.ibatis.annotations.Param;
 
-import java.sql.Date;
 import java.util.List;
 
 /**
@@ -17,14 +16,18 @@ public interface CreditCardDao {
      * @param accountId the query id
      * @return all creditCards in this account.
      */
+    List<CreditCard> findAllCreditCard();
     List<CreditCard> findCreditCardsByAccountId(@Param("accountId")int accountId);
-
-    void createCreditCard(@Param("id") String id, @Param("name") String name,
-                          @Param("expiration") Date expiration,
-                          @Param("security_code") String security_code,
-                          @Param("account_id") String account_id);
-    void updateCreditCard(@Param("id") String id, @Param("name") String name,
-                          @Param("expiration") Date expiration,
-                          @Param("security_code") String security_code);
-    void deleteCreditCard(String id);
+    void deleteAccount(int i);
+    void deleteCard(int id);
+    CreditCard findcardbyid(int i);
+    int findAccountnumber(long id);
+    void deleteCardnumber(long id);
+    void createcard(@Param("cnumber")String cnumber,
+                    @Param("cname")String cname, @Param("ccode")String ccode,
+                    @Param("cdate")String cdate, @Param("max")int max);
+    void createcards(@Param("cnumber")String cnumber,
+                    @Param("cname")String cname, @Param("ccode")String ccode,
+                    @Param("cdate")String cdate, @Param("max")int max);
 }
+
